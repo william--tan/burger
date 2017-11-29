0c931d139f4346f4e9bb47aebf773a78456b12b8
william--tan <william.tan@biola.edu>
2017-11-29 13:59:41 -0800

    changed connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,18 @@
 const mysql = require('mysql')
 
+// let connection = mysql.createConnection({
+// 	port: 3306,
+// 	host: 'localhost',
+// 	user: 'root',
+// 	password: '',
+// 	database: 'burgers_db'
+// })
+
 let connection = mysql.createConnection({
-	port: 3306,
-	host: 'localhost',
-	user: 'root',
-	password: '',
-	database: 'burgers_db'
+	host:'us-cdbr-iron-east-05.cleardb.net',
+	user: 'bb55da9d68bcc7',
+	password: '82a9ed47c6b1974',
+	database: 'heroku_c95269d4e05feb3'
 })
 
 connection.connect(err => {
