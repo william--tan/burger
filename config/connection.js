cf4dec785c395d046460900d650adf6b59dce035
william--tan <william.tan@biola.edu>
2017-11-29 15:25:10 -0800

    changed to Pool instead of single connection

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ const mysql = require('mysql')
 // 	database: 'burgers_db'
 // })
 
-let connection = mysql.createConnection({
+let connection = mysql.createPool({
 	host:'us-cdbr-iron-east-05.cleardb.net',
 	user: 'bb55da9d68bcc7',
 	password: '82a9ed47c6b1974',
