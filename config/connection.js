0db98c6dbd95332c9f565d2a55131cd04dad7d3f
william--tan <william.tan@biola.edu>
2017-11-29 15:37:33 -0800

    added disconnect handling

--- config/connection.js
+++ config/connection.js
@@ -8,16 +8,34 @@ const mysql = require('mysql')
 // 	database: 'burgers_db'
 // })
 
-var pool = mysql.createPool({
-	connectionLimit: 20,
+var connection = mysql.createConnection({
 	host:'us-cdbr-iron-east-05.cleardb.net',
 	user: 'bb55da9d68bcc7',
 	password: '82a9ed47c6b1974',
 	database: 'heroku_c95269d4e05feb3'
 })
 
-pool.getConnection( (err, connection) => {
+connection.connect(err => {
 	if (err) console.log(err);
 	console.log("Connected as ID " + connection.threadId);
-	module.exports = connection;
 })
+
+handleDisconnect(connection);
+
+function handleDisconnect(client) {
+  client.on('error', function (error) {
+    if (!error.fatal) return;
+    if (error.code !== 'PROTOCOL_CONNECTION_LOST') throw err;
+
+    console.error('> Re-connecting lost MySQL connection: ' + error.stack);
+
+    // NOTE: This assignment is to a variable from an outer scope; this is extremely important
+    // If this said `client =` it wouldn't do what you want. The assignment here is implicitly changed
+    // to `global.mysqlClient =` in node.
+    mysqlClient = mysql.createConnection(client.config);
+    handleDisconnect(mysqlClient);
+    mysqlClient.connect();
+  });
+};
+
+module.exports = connection;
