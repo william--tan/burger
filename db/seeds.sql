06a538c644b249463dbf78bb7b49d3853e9c2c5d
william--tan <william.tan@biola.edu>
2017-11-29 14:49:35 -0800

    modified seed

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,13 @@
+DROP TABLE burgers;
+CREATE TABLE burgers
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(50) NOT NULL,
+	devoured BOOLEAN NOT NULL DEFAULT 0,
+	date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY(id)
+);
+
 INSERT INTO burgers(name) VALUES ("Bourbon Bacon Burger");
 INSERT INTO burgers(name) VALUES ("Truffle Burger");
 INSERT INTO burgers(name) VALUES ("Spicy Deluxe Chicken Sandwich");
