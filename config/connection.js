c3a04583115eb9f3e8c1cee1bed09e9b38197409
william--tan <william.tan@biola.edu>
2017-11-29 16:21:47 -0800

    use pool

--- config/connection.js
+++ config/connection.js
@@ -15,29 +15,57 @@ var config = {
 	database: 'heroku_c95269d4e05feb3'
 };
 
-var connection = mysql.createConnection(config)
+var pool = mysql.createPool(config)
 
-connection.connect(err => {
-	if (err) console.log(err);
-	console.log("Connected as ID " + connection.threadId);
-})
+// connection.connect(err => {
+// 	if (err) console.log(err);
+// 	console.log("Connected as ID " + connection.threadId);
+// })
 
-handleDisconnect(connection);
+// handleDisconnect(connection);
 
-function handleDisconnect(client) {
-  client.on('error', function (error) {
-    if (!error.fatal) return;
-    if (error.code !== 'PROTOCOL_CONNECTION_LOST') throw err;
+// function handleDisconnect(client) {
+//   client.on('error', function (error) {
+//     if (!error.fatal) return;
+//     if (error.code !== 'PROTOCOL_CONNECTION_LOST') throw err;
 
-    console.error('> Re-connecting lost MySQL connection: ' + error.stack);
+//     console.error('> Re-connecting lost MySQL connection: ' + error.stack);
 
-    // NOTE: This assignment is to a variable from an outer scope; this is extremely important
-    // If this said `client =` it wouldn't do what you want. The assignment here is implicitly changed
-    // to `global.mysqlClient =` in node.
-    mysqlClient = mysql.createConnection(config);
-    handleDisconnect(mysqlClient);
-    mysqlClient.connect();
-  });
-};
+//     // NOTE: This assignment is to a variable from an outer scope; this is extremely important
+//     // If this said `client =` it wouldn't do what you want. The assignment here is implicitly changed
+//     // to `global.mysqlClient =` in node.
+//     mysqlClient = mysql.createConnection(config);
+//     handleDisconnect(mysqlClient);
+//     mysqlClient.connect();
+//   });
+// };
 
 module.exports = connection;
+
+module.exports = {
+    query: function(){
+        var sql_args = [];
+        var args = [];
+        for(var i=0; i<arguments.length; i++){
+            args.push(arguments[i]);
+        }
+        var callback = args[args.length-1]; //last arg is callback
+        pool.getConnection(function(err, connection) {
+        if(err) {
+                console.log(err);
+                return callback(err);
+            }
+            if(args.length > 2){
+                sql_args = args[1];
+            }
+        connection.query(args[0], sql_args, function(err, results) {
+          connection.release(); // always put connection back in pool after last query
+          if(err){
+                    console.log(err);
+                    return callback(err);
+                }
+          callback(null, results);
+        });
+      });
+    }
+};
